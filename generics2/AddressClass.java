e0e9ea002673de6bd9eb9d2bbe8df72914a4276a
Saurav-44 <138797337+Saurav-44@users.noreply.github.com>
2024-03-17 14:02:05 +0530

    Add files via upload

---
+++ generics2/AddressClass.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class AddressClass {
+    public static void main(String[] args) {
+        TreeMap<String, Address> tm = new TreeMap<>();
+        tm.put("Saurav", new Address("B32", "Rairangpur", "Bahalda"));
+        tm.put("Som", new Address("B31", "Kucheibudi", "Baripada"));
+
+        Iterator<String> it = tm.keySet().iterator();
+        while(it.hasNext()) {
+            String name = it.next();
+            Address ad = tm.get(name);
+            System.out.println("Name = " + name + "," + " Address = " + ad);
+        }
+    }
+}
+
+class Address {
+    String plotNo;
+    String at;
+    String post;
+
+    public Address(String plotNo, String at, String post) {
+        this.plotNo = plotNo;
+        this.at = at;
+        this.post = post;
+    }
+
+    @Override
+    public String toString() {
+        return "Plot.No: " + plotNo + "," + " at: " + at + "," + " post: " + post;
+    }
+}
